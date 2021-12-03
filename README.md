aaefd04bad86b736f13f03df65754627a8f6c85b
German Middi SAP-BAS <german.middi@gmail.com>
2021-12-03 13:33:31 +0000

    Local json model

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+## Employees - Formulario Fiori con SAPUI5
 ## Application Details
 |               |
 | ------------- |
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-],
+	'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+    ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
 	 */
-	function (Controller) {
+	function (Controller, JSONModel) {
 		"use strict";
 
 		return Controller.extend("logaligroup.front.controller.View1", {
 			onInit: function () {
 
+                const oModel = new JSONModel();
+                oModel.loadData("./localSevice/mockdata/CustomerModel.json");
+
+                this.oView = this.getView();
+                this.oView.setModel(oModel);
+
+
 			}
 		});
-	});
+	}
+);
